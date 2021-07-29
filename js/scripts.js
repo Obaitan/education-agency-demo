819685e92e2b1f6fce2ab804157a0e67e4ca5931
Obaitan <elero.obaitan@gmail.com>
2021-07-29 11:21:30 +0100

    Initialized project.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Sticky Top Nav
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+            document.getElementById("header").classList.add("sticky-top");
+        } else {
+            document.getElementById("header").classList.remove("sticky-top");
+        }
+    });
+});
+
+// Animate menu icon
+function myFunction(x) {
+    x.classList.toggle("change");
+}
